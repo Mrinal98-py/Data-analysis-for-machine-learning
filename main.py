f64d9dd67684c0b9ba1871715e0f5d248d45fe0d
mrinal98-py <mrinal98raj@gmail.com>
2024-07-09 23:47:01 +0530

    t

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
-import math as m
-a = []
-print("re      ")
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import warnings
+from six.moves import urllib
+import os
+
+warnings.formatwarning("ignore_warning")
+%matplotlib inline
+
+data_dir = ""   #dir of dataset
+
